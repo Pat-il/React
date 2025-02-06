b6ca33e45afa4e4d0b21cc6e3ccb5ea897895c98
Tejas <90674200+Pat-il@users.noreply.github.com>
2025-02-06 15:29:05 +0530

    The Calculator UI Created

---
+++ Calculator-1/src/components/Display.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Display.module.css'
+
+function Display() {
+  return (
+    <div>
+      <input className={styles.display} type="text"></input>
+    </div>
+  )
+}
+
+export default Display
