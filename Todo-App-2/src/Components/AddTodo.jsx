a0ade15929cccc7ebcd224bb8481e881a2e27c85
Tejas <90674200+Pat-il@users.noreply.github.com>
2025-02-10 19:44:59 +0530

    Using form how we render data

--- Todo-App-2/src/Components/AddTodo.jsx
+++ Todo-App-2/src/Components/AddTodo.jsx
@@ -40,3 +40,60 @@ function AddTodo({onNewItem}) {
 }
 
 export default AddTodo
+
+
+// --------------------------------------------------------------------------
+
+// Using the Form how we store data
+
+// import React from "react";
+// import { IoBagAdd } from "react-icons/io5";
+// import { useState } from "react";
+
+// function AddTodo({ onNewItem }) {
+//   const [todoName, setTodoName] = useState("");
+//   const [todoDate, setDueDate] = useState("");
+
+//   const handleNameChange = (event) => {
+//     setTodoName(event.target.value);
+//   };
+
+//   const handleDateChange = (event) => {
+//     setDueDate(event.target.value);
+//   };
+
+//   const handleAddButtonClicked = (event) => {
+//     console.log(event);
+//     event.preventDefault();
+//     onNewItem(todoName, todoDate);
+//     setTodoName("");
+//     setDueDate("");
+//   };
+
+//   return (
+//     <div className="container text-center">
+//       <form className="row kg-row" 
+//           onSubmit={handleAddButtonClicked}>
+//         <div className="col-6">
+//           <input
+//             type="text"
+//             onChange={handleNameChange}
+//             placeholder="Enter Todo Here"
+//           />
+//         </div>
+//         <div className="col-4">
+//           <input onChange={handleDateChange} type="date" />
+//         </div>
+//         <div className="col-2">
+//           <button
+//             className="btn btn-success kg-button"
+//           >
+//             <IoBagAdd />
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default AddTodo;
