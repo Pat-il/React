c74bb39fdfee830ddda09eaf2bb33add5a2e9bc6
Tejas <90674200+Pat-il@users.noreply.github.com>
2025-02-10 22:12:38 +0530

    Changing essesial Code

--- Bharat-Clock-1/src/Components/ClockSlogan.jsx
+++ Bharat-Clock-1/src/Components/ClockSlogan.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ClockSlogan() {
   return (
     <div>
-      <p className='class="fs-4"'>This is the Clock that shows the Time in Bharay at All Times</p>
+      <p className='class="fs-4"'>This is the Clock that shows the Time in Bharat at All Times</p>
     </div>
   )
 }
--- Bharat-Clock-1/src/Components/CurrentTime.jsx
+++ Bharat-Clock-1/src/Components/CurrentTime.jsx
@@ -3,11 +3,10 @@ import React from 'react'
 function CurrentTime() {
 
   const currDate = new Date();
-  const currTime = new Date();
 
   return (
     <div>
-      <p className='class="fs-4"'>This is the Current Time: {currDate.toLocaleDateString()} : {currTime.toLocaleTimeString()}</p>
+      <p className='class="fs-4"'>This is the Current Time: {currDate.toLocaleDateString()} : {currDate.toLocaleTimeString()}</p>
     </div>
   )
 }
